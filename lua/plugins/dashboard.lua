daa36dd4e5a21a1c20c01beb84e1a5dcd2fb2f9c
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2024-10-03 13:08:37 +0100

    chore: remove unnecessary function

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -3,7 +3,6 @@ return {
   opts = function(_, opts)
     local tenet = "We work in the dark to serve the light"
 
-    -- Logo
     local logo = string.format(
       [[
     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
@@ -34,42 +33,18 @@ return {
     logo = string.rep("\n", 8) .. logo .. "\n\n"
     opts.config.header = vim.split(logo, "\n")
 
-    local quote = [[
-When I was a young man, I had liberty, but I did not see it. I had time,
-but I did not know it. And I had love, but I did not feel it.
-
-- Ezio Auditore]]
-
-    local function wrap_text(text, width)
-      local wrapped = {}
-      local line = ""
-      for word in text:gmatch("%S+") do
-        if #line + #word + 1 > width then
-          table.insert(wrapped, line)
-          line = word
-        else
-          line = #line == 0 and word or line .. " " .. word
-        end
-      end
-      if #line > 0 then
-        table.insert(wrapped, line)
-      end
-      return wrapped
-    end
-
     opts.config.footer = function()
       local stats = require("lazy").stats()
       local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-      local wrapped_quote = wrap_text(quote, 80)
 
       local footer = {
         "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms",
         "",
+        "When I was a young man, I had liberty, but I did not see it. I had time,",
+        "but I did not know it. And I had love, but I did not feel it.",
+        "",
+        "- Ezio Auditore",
       }
-
-      for _, line in ipairs(wrapped_quote) do
-        table.insert(footer, line)
-      end
 
       return footer
     end
