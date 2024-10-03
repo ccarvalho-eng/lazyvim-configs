7a753a25044fb954c1d58b182f954bf1bc44377f
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2024-10-04 00:40:21 +0100

    feat: install wiki

---
+++ lua/plugins/wiki.lua
@@ -0,0 +1,15 @@
+return {
+  "vimwiki/vimwiki",
+  event = "BufEnter *.md",
+  keys = { "<leader>ww", "<leader>wt" },
+  init = function()
+    vim.g.vimwiki_list = {
+      {
+        path = "~/Library/Mobile Documents/com~apple~CloudDocs/vimwiki/",
+        syntax = "markdown",
+        ext = "md",
+      },
+    }
+    -- vim.g.vimwiki_ext2syntax = {}
+  end,
+}
