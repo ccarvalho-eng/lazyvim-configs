533a3cb7032079df5be53276b1beee37e35884f2
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2024-10-08 12:16:07 +0100

    chore: add git fugitive

---
+++ lua/plugins/git.lua
@@ -0,0 +1,14 @@
+return {
+
+  {
+    "tpope/vim-fugitive",
+    cmd = "Git",
+    keys = {
+      { "<leader>gt", ":.GBrowse<CR>", desc = "Open current line on GithHub" },
+    },
+  },
+  {
+    "tpope/vim-rhubarb",
+    dependencies = { "tpope/vim-fugitive" },
+  },
+}
