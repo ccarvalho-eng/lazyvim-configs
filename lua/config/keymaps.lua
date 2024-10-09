10ac1bc804f5a551846c19ae1ffefd055d1c2ff2
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2024-10-09 15:01:40 +0100

    chore: configure terminal keybinding

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Open terminal in a horizontal split with a specified height
+vim.api.nvim_set_keymap("n", "<leader>th", ":split | resize 20 | terminal<CR>", { noremap = true, silent = true })
+
+-- Open terminal in a vertical split with a specified width
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>tv",
+  ":vsplit | vertical resize 80 | terminal<CR>",
+  { noremap = true, silent = true }
+)
