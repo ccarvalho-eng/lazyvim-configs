ee70e80f21f1de30da329f7bd79e9ac1a0bd6c96
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2024-10-23 12:33:38 +0100

    chore: set light theme for the day

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,11 +3,15 @@ return {
     "navarasu/onedark.nvim",
     lazy = false,
     priority = 1000,
-    opts = {
-      style = "dark", -- Choose the style: dark, darker, cool, deep, warm, warmer
-    },
-    config = function(_, opts)
-      require("onedark").setup(opts)
+    config = function()
+      -- Get current hour (0-23)
+      local hour = tonumber(os.date("%H"))
+      -- Set light theme during day (e.g., 6 AM to 6 PM)
+      local is_daytime = hour >= 6 and hour < 18
+
+      require("onedark").setup({
+        style = is_daytime and "light" or "dark",
+      })
       vim.cmd.colorscheme("onedark")
     end,
   },
