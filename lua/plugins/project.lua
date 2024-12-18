30b8fce5c1922a67c662d201aafb6a2f99a58546
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2024-12-18 12:29:13 +0000

    chore: remove telescope

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -1,5 +1,8 @@
 return {
   "ahmedkhalf/project.nvim",
+  dependencies = {
+    "ibhagwan/fzf-lua",
+  },
   opts = {
     detection_methods = { "pattern" },
     patterns = {
@@ -37,6 +40,17 @@ return {
   },
   config = function(_, opts)
     require("project_nvim").setup(opts)
-    require("telescope").load_extension("projects")
+    local history = require("project_nvim.utils.history")
+    history.delete_project = function(project)
+      for k, v in pairs(history.recent_projects) do
+        if v == project.value then
+          history.recent_projects[k] = nil
+          return
+        end
+      end
+    end
+    LazyVim.on_load("fzf-lua.nvim", function()
+      require("fzf-lua").load_extension("projects")
+    end)
   end,
 }
