a350aad5c38226c4b263bcbe26d8a0331598d25c
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2024-12-22 00:40:35 +0000

    chore: improve vim journal configs

--- lua/plugins/journal.lua
+++ lua/plugins/journal.lua
@@ -4,6 +4,23 @@ return {
     config = function()
       require("journal").setup({
         root = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/Animus/journal",
+        journal = {
+          entries = {
+            -- Entry type for each quarter of the year
+            quarter = {
+              -- strftime doesn't supply a quarter variable, so we compute it manually
+              format = function(date)
+                local quarter = math.ceil(tonumber(os.date("%m", os.time(date.date))) / 3)
+                return "%Y/quarter/" .. quarter
+              end,
+              template = function(date)
+                local quarter = math.ceil(os.date("%m", os.time(date.date)) / 3)
+                return "# %Y Quarter " .. quarter .. "\n"
+              end,
+              frequency = { month = 3 },
+            },
+          },
+        },
       })
     end,
   },
