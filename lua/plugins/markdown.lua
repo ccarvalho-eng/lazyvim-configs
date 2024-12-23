f65d1e6056a806c6610592993347bc3fd5101cad
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2024-12-23 12:39:56 +0000

    chore: add markdown preview

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -44,6 +44,25 @@ return {
     end,
   },
   {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = function()
+      require("lazy").load({ plugins = { "markdown-preview.nvim" } })
+      vim.fn["mkdp#util#install"]()
+    end,
+    keys = {
+      {
+        "<leader>cp",
+        ft = "markdown",
+        "<cmd>MarkdownPreviewToggle<cr>",
+        desc = "Markdown Preview",
+      },
+    },
+    config = function()
+      vim.cmd([[do FileType]])
+    end,
+  },
+  {
     "stevearc/conform.nvim",
     optional = true,
     opts = {
