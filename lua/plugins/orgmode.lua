82d27cf56340819562461b1361e8cb701eb7f0cc
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2025-01-25 09:46:25 +0000

    chore: some plugins to improve orgmode

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -1,22 +1,37 @@
 return {
-  "nvim-orgmode/orgmode",
-  event = "VeryLazy",
-  ft = { "org" },
-  config = function()
-    -- Setup orgmode
-    local agenda_dir = os.getenv("ORG_AGENDA_FILES")
-    require("orgmode").setup({
-      org_agenda_files = { agenda_dir .. "/*.org" },
-      org_default_notes_file = agenda_dir .. "/notes.org",
-    })
+  {
+    "nvim-orgmode/orgmode",
+    event = "VeryLazy",
+    ft = { "org" },
+    config = function()
+      local agenda_dir = os.getenv("ORG_AGENDA_FILES")
+      require("orgmode").setup({
+        org_agenda_files = { agenda_dir .. "/*.org" },
+        org_default_notes_file = agenda_dir .. "/notes.org",
+      })
 
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = "all",
-      ignore_install = { "org" },
-      -- Add missing required fields
-      modules = {},
-      sync_install = false,
-      auto_install = true,
-    })
-  end,
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = "all",
+        ignore_install = { "org" },
+        modules = {},
+        sync_install = false,
+        auto_install = true,
+      })
+    end,
+  },
+  {
+    "akinsho/org-bullets.nvim",
+    ft = { "org" },
+    config = function()
+      require("org-bullets").setup()
+    end,
+  },
+  {
+    "dhruvasagar/vim-table-mode",
+    ft = { "org" },
+    config = function()
+      vim.g.table_mode_map_prefix = "<leader>tm"
+      vim.g.table_mode_corner = "+"
+    end,
+  },
 }
