1118de03c7ee84cd57c6f70dc64e6c5d12713051
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2025-01-28 12:37:51 +0000

    chore: install neorg

---
+++ lua/plugins/neorg.lua
@@ -0,0 +1,51 @@
+return {
+  {
+    "nvim-neorg/neorg",
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = "Neorg",
+    ft = "norg",
+    opts = function()
+      local root_dir = os.getenv("NEORG_NOTES")
+      return {
+        load = {
+          ["core.defaults"] = {},
+          ["core.concealer"] = {},
+          ["core.dirman"] = {
+            config = {
+              workspaces = {
+                journal = root_dir,
+                notes = root_dir,
+              },
+              default_workspace = "notes",
+            },
+          },
+          ["core.completion"] = {
+            config = {
+              engine = "nvim-cmp",
+            },
+          },
+          ["core.journal"] = {
+            config = {
+              strategy = "flat",
+              workspace = "journal",
+            },
+          },
+          ["core.summary"] = {},
+          ["core.export"] = {},
+          ["core.export.markdown"] = {
+            config = {
+              extension = "md",
+            },
+          },
+          ["core.keybinds"] = {
+            config = {
+              default_keybinds = true,
+              neorg_leader = "<Leader>n",
+            },
+          },
+        },
+      }
+    end,
+  },
+}
