42af48db5454fa7f6549457580c2de8cb900493f
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2025-04-09 16:19:05 +0100

    chore: add keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,21 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Open terminal in a horizontal split with a specified height
+vim.api.nvim_set_keymap("n", "<leader>th", ":split | resize 20 | terminal<CR>", { noremap = true, silent = true })
+
+-- Open terminal in a vertical split with a specified width
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>tv",
+  ":vsplit | vertical resize 80 | terminal<CR>",
+  { noremap = true, silent = true }
+)
+
+-- Switch between buffers
+vim.api.nvim_set_keymap("n", "gt", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "gT", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
+
+-- Keybinding to delete all buffers
+vim.keymap.set("n", "<leader>bK", ":%bd<CR>", { noremap = true, silent = true, desc = "Close all buffers" })
