0b8ea0a07f4c78b2b3e6cc9876034bd4c3818151
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2025-04-11 16:33:37 +0100

    chore: change copilot user emoji

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -34,7 +34,7 @@ return {
       user = user:sub(1, 1):upper() .. user:sub(2)
       return {
         auto_insert_mode = true,
-        question_header = "🥷🏻  " .. user .. " ",
+        question_header = "🧙🏽‍♂️  " .. user .. " ",
         answer_header = "  Copilot ",
         window = {
           width = 0.4,
