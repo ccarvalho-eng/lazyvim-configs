b6cd01377b31af18c9abe61820125c67a042d9b1
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2025-04-14 02:52:10 +0100

    chore: load diagram within lazyvim

--- init.lua
+++ init.lua
@@ -1,10 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+-- Exports Mermaid diagrams on save
+
 local function ensure_mermaid_theme(file_path)
   local lines = vim.fn.readfile(file_path)
   local theme_header =
-    '%%{init: { "theme": "dark", "themeVariables": { "background": "#1e1e1e", "primaryColor": "#bb86fc" }}}%%'
+    '%%{init: { "theme": "dark", "themeVariables": { "background": "transparent", "primaryColor": "#bb86fc" }}}%%'
 
   if not vim.startswith(lines[1], "%%{init:") then
     table.insert(lines, 1, theme_header)
@@ -12,7 +14,6 @@ local function ensure_mermaid_theme(file_path)
   end
 end
 
--- Exports Mermaid diagrams on save
 vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = "*.mmd",
   callback = function()
@@ -34,14 +35,14 @@ vim.api.nvim_create_autocmd("BufWritePost", {
       "-H",
       height,
       "--backgroundColor",
-      "#1e1e1e",
+      "transparent",
     }, {
       on_exit = function(_, code)
         if code == 0 then
           print("Mermaid diagram exported to " .. output_file)
           local win_id = vim.fn.bufwinid(output_file)
           if win_id == -1 then
-            vim.fn.jobstart({ "open", output_file }) -- macOS-specific
+            vim.cmd("vsplit " .. output_file)
           else
             vim.api.nvim_set_current_win(win_id)
           end
