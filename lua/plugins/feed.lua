df2e3c9d6373a8e3436c80cedb88262b802ac10f
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2025-06-20 02:13:31 +0100

    feat: install fead reader

---
+++ lua/plugins/feed.lua
@@ -0,0 +1,61 @@
+return {
+  "neo451/feed.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  opts = {
+    feeds = {
+      -- News
+      "https://feeds.bbci.co.uk/news/world/rss.xml",
+      "http://feeds.reuters.com/reuters/topNews",
+      "https://www.theguardian.com/international/rss",
+      "https://apnews.com/rss",
+      "https://rss.dw.com/rdf/rss-en-all",
+
+      -- Cybersecurity
+      "https://krebsonsecurity.com/feed/",
+      "https://feeds.feedburner.com/TheHackersNews",
+      "https://www.bleepingcomputer.com/feed/",
+      "https://threatpost.com/feed/",
+      "https://nvd.nist.gov/feeds/xml/cve/misc/nvd-rss.xml",
+      "https://www.schneier.com/blog/atom.xml",
+      "https://www.darkreading.com/rss.xml",
+
+      -- Tech & Programming Communities
+      "https://news.ycombinator.com/rss",
+      "https://lobste.rs/rss",
+      "https://arstechnica.com/feed/",
+      "https://www.technologyreview.com/feed/",
+      "https://feeds.feedburner.com/oreilly/radar/atom",
+
+      -- Gaming
+      "https://www.pcgamer.com/rss/",
+      "https://www.rockpapershotgun.com/feed",
+      "https://www.eurogamer.net/?format=rss",
+      "https://kotaku.com/rss",
+
+      -- Literature & Books
+      "https://lithub.com/feed/",
+      "https://www.nybooks.com/feed/",
+      "https://www.theparisreview.org/blog/feed/",
+      "https://www.tor.com/feed/",
+
+      -- Elixir & Erlang
+      "https://elixir-lang.org/blog/feed.xml",
+      "https://www.erlang.org/rss.xml",
+      "https://blog.erlang.org/feed.xml",
+      "https://dashbit.co/feed",
+
+      -- Personal Development
+      "https://zenhabits.net/feed/",
+      "https://www.mindful.org/feed/",
+      "https://www.raptitude.com/feed/",
+    },
+  },
+
+  -- Key mappings for easy access
+  keys = {
+    { "<leader>rf", "<cmd>Feed<cr>", desc = "Open RSS Feed Reader" },
+  },
+}
