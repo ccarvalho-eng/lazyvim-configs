7084b3098c0639ed2c1276f0c62b2b87dd572bcf
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2025-06-20 03:07:37 +0100

    chore: tag all feeds and update deps

--- lua/plugins/feed.lua
+++ lua/plugins/feed.lua
@@ -3,55 +3,62 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope.nvim",
-    "neo451/snacks.nvim",
   },
   opts = {
     feeds = {
       -- News
-      "https://feeds.bbci.co.uk/news/world/rss.xml",
-      "https://news.google.com/rss/search?q=site%3Areuters.com&hl=en-US&gl=US&ceid=US%3A",
-      "https://www.theguardian.com/international/rss",
-      "https://apnews.com/rss",
-      "https://rss.dw.com/rdf/rss-en-all",
+      { "https://feeds.bbci.co.uk/news/world/rss.xml", name = "BBC News", tags = { "news", "world" } },
+      {
+        "https://news.google.com/rss/search?q=site%3Areuters.com&hl=en-US&gl=US&ceid=US%3A",
+        name = "Reuters via Google",
+        tags = { "news", "world" },
+      },
+      { "https://www.theguardian.com/international/rss", name = "The Guardian", tags = { "news", "world" } },
+      { "https://apnews.com/rss", name = "AP News", tags = { "news", "world" } },
+      { "https://rss.dw.com/rdf/rss-en-all", name = "Deutsche Welle", tags = { "news", "world" } },
 
       -- Cybersecurity
-      "https://krebsonsecurity.com/feed/",
-      "https://feeds.feedburner.com/TheHackersNews",
-      "https://www.bleepingcomputer.com/feed/",
-      "https://threatpost.com/feed/",
-      "https://nvd.nist.gov/feeds/xml/cve/misc/nvd-rss.xml",
-      "https://www.schneier.com/blog/atom.xml",
-      "https://www.darkreading.com/rss.xml",
+      { "https://krebsonsecurity.com/feed/", name = "Krebs on Security", tags = { "security", "tech" } },
+      { "https://feeds.feedburner.com/TheHackersNews", name = "The Hackers News", tags = { "security", "tech" } },
+      { "https://www.bleepingcomputer.com/feed/", name = "Bleeping Computer", tags = { "security", "tech" } },
+      { "https://threatpost.com/feed/", name = "Threatpost", tags = { "security", "tech" } },
+      {
+        "https://nvd.nist.gov/feeds/xml/cve/misc/nvd-rss.xml",
+        name = "NVD Vulnerabilities",
+        tags = { "security", "cve" },
+      },
+      { "https://www.schneier.com/blog/atom.xml", name = "Schneier on Security", tags = { "security", "privacy" } },
+      { "https://www.darkreading.com/rss.xml", name = "Dark Reading", tags = { "security", "tech" } },
 
       -- Tech & Programming Communities
-      "https://news.ycombinator.com/rss",
-      "https://lobste.rs/rss",
-      "https://arstechnica.com/feed/",
-      "https://www.technologyreview.com/feed/",
-      "https://feeds.feedburner.com/oreilly/radar/atom",
+      { "https://news.ycombinator.com/rss", name = "Hacker News", tags = { "tech", "programming", "community" } },
+      { "https://lobste.rs/rss", name = "Lobsters", tags = { "tech", "programming", "community" } },
+      { "https://arstechnica.com/feed/", name = "Ars Technica", tags = { "tech", "news" } },
+      { "https://www.technologyreview.com/feed/", name = "MIT Technology Review", tags = { "tech", "innovation" } },
+      { "https://feeds.feedburner.com/oreilly/radar/atom", name = "O'Reilly Radar", tags = { "tech", "programming" } },
 
       -- Gaming
-      "https://www.pcgamer.com/rss/",
-      "https://www.rockpapershotgun.com/feed",
-      "https://www.eurogamer.net/?format=rss",
-      "https://kotaku.com/rss",
+      { "https://www.pcgamer.com/rss/", name = "PC Gamer", tags = { "gaming", "pc" } },
+      { "https://www.rockpapershotgun.com/feed", name = "Rock Paper Shotgun", tags = { "gaming", "pc" } },
+      { "https://www.eurogamer.net/?format=rss", name = "Eurogamer", tags = { "gaming" } },
+      { "https://kotaku.com/rss", name = "Kotaku", tags = { "gaming" } },
 
       -- Literature & Books
-      "https://lithub.com/feed/",
-      "https://www.nybooks.com/feed/",
-      "https://www.theparisreview.org/blog/feed/",
-      "https://www.tor.com/feed/",
+      { "https://lithub.com/feed/", name = "Literary Hub", tags = { "books", "literature" } },
+      { "https://www.nybooks.com/feed/", name = "NY Review of Books", tags = { "books", "literature" } },
+      { "https://www.theparisreview.org/blog/feed/", name = "The Paris Review", tags = { "books", "literature" } },
+      { "https://www.tor.com/feed/", name = "Tor.com", tags = { "books", "scifi", "fantasy" } },
 
       -- Elixir & Erlang
-      "https://elixir-lang.org/blog/feed.xml",
-      "https://www.erlang.org/rss.xml",
-      "https://blog.erlang.org/feed.xml",
-      "https://dashbit.co/feed",
+      { "https://elixir-lang.org/blog/feed.xml", name = "Elixir Blog", tags = { "programming", "elixir" } },
+      { "https://www.erlang.org/rss.xml", name = "Erlang.org", tags = { "programming", "erlang" } },
+      { "https://blog.erlang.org/feed.xml", name = "Erlang Blog", tags = { "programming", "erlang" } },
+      { "https://dashbit.co/feed", name = "Dashbit", tags = { "programming", "elixir" } },
 
       -- Personal Development
-      "https://zenhabits.net/feed/",
-      "https://www.mindful.org/feed/",
-      "https://www.raptitude.com/feed/",
+      { "https://zenhabits.net/feed/", name = "Zen Habits", tags = { "mindfulness", "personal-development" } },
+      { "https://www.mindful.org/feed/", name = "Mindful", tags = { "mindfulness", "personal-development" } },
+      { "https://www.raptitude.com/feed/", name = "Raptitude", tags = { "mindfulness", "personal-development" } },
     },
   },
 
