2a4ab1a6763ab52c07c8390c347eecd7a6b0f104
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2025-10-25 11:05:57 +0100

    chore: update deps, add transparency to theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,7 +8,10 @@ return {
       require("onedark").setup({
         -- Customize the theme here (optional)
         style = "dark", -- Options: dark, darker, cool, deep, warm, warmer
+        transparent = true,
       })
+      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end,
   },
 
